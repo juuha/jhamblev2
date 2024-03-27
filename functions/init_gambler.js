5f7aa8b80863e40fb958c85b4162f1e2af3cabe5
Juha Ritakoski <juha.ritakoski@hotmail.com>
2024-03-27 23:16:34 +0200

    add fallback to username if no globalName

--- functions/init_gambler.js
+++ functions/init_gambler.js
@@ -5,7 +5,7 @@ module.exports = async (client, gambler) => {
     if (!gamblers[gambler.id]) {
         gamblers[gambler.id] = {
             id: gambler.id,
-            name: gambler.globalName,
+            name: gambler.globalName || gambler.username,
             ecto: 1250,
             gold: 500,
             free: 18706,
