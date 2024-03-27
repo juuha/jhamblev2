9fed2e0fb3820ea85d0fd6d9205776f918f72b74
Juha Ritakoski <juha.ritakoski@hotmail.com>
2024-03-27 20:42:41 +0200

    add sell command

---
+++ commands/sell.js
@@ -0,0 +1,51 @@
+const { SlashCommandBuilder } = require('discord.js');
+const init_gambler = require('../functions/init_gambler.js');
+const update_gambler = require('../functions/update_gambler.js');
+const init_emojis = require("../functions/init_emojis.js");
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('sell')
+		.setDescription('Sell Ectos or Orbs.')
+        .addIntegerOption(option =>
+            option.setName("amount")
+                .setMinValue(1)
+                .setRequired(true)
+                .setDescription("How many you want to sell.")
+        )
+        .addStringOption(option =>
+            option.setName("what")
+            .setRequired(true)
+            .setDescription("What you want to sell.")
+            .addChoices(
+                { name: "Ectos", value: "ecto" },
+                { name: "Orbs", value: "orb" },
+            )),
+	async execute(interaction, client) {
+        let what = interaction.options.getString("what");
+        let amount = interaction.options.getInteger("amount");
+
+        let gambler = await init_gambler(client, interaction.user);
+        let emojis = await init_emojis(client);
+
+        if (gambler[what] < amount) {
+            let error_message = `You only have ${gambler[what]} ${emojis[what]}.`;
+            try {
+                await interaction.reply({ content: error_message, ephemeral: true });
+            } catch (error) { console.log(error) }
+            return;
+        }
+
+        price = { "ecto": 0.4, "orb": 100 };
+
+        gambler[what] -= amount;
+        gambler.gold += amount * price[what];
+
+        await update_gambler(gambler);
+
+        let message = `${amount} ${emojis[what]} sold for ${amount * price[what]} ${emojis.gold}.`;
+        try {
+            const sent = await interaction.reply({ content: message, ephemeral: true });
+        } catch (error) { console.error(error) }
+    }
+}
