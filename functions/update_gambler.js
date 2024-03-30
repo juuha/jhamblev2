8f4f68fa5b4aa00a793e42d415ef2c23f9318fc2
Juha Ritakoski <juha.ritakoski@hotmail.com>
2024-03-30 16:11:56 +0200

    fix semicolons

--- functions/update_gambler.js
+++ functions/update_gambler.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 module.exports = async (gambler) => {
     let gamblers = require('../gamblers.json');
     gamblers[gambler.id] = gambler;
-    fs.writeFile('./gamblers.json', JSON.stringify(gamblers, null, 4), async (error) => {;
+    fs.writeFile('./gamblers.json', JSON.stringify(gamblers, null, 4), async (error) => {
         if (error) console.error(error);
-    })
+    });
 }
