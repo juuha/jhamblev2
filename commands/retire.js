c51b0a2a3c46d5c35ec0e58f66cff7d4e9dc538e
Juha Ritakoski <juha.ritakoski@hotmail.com>
2024-03-30 15:40:42 +0200

    add retire command

---
+++ commands/retire.js
@@ -0,0 +1,33 @@
+const { SlashCommandBuilder } = require('discord.js');
+const update_gambler = require('../functions/update_gambler');
+const init_gambler = require("../functions/init_gambler.js");
+const gamblers = require('../gamblers.json');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('retire')
+		.setDescription('Retire a player. (They will come out of retirement if they use any Jhamble command)')
+        .addUserOption(option =>
+            option.setName("retiree")
+                .setRequired(true)
+                .setDescription("Who you want to retire.")
+            ),
+	async execute(interaction, client) {
+		let who = interaction.options.getUser("retiree");
+        let retiree = {}
+
+        if (gamblers[who.id]) {
+            retiree = gamblers[who.id]
+        } else {
+            retiree = await init_gambler(client, who);
+        }
+        
+        retiree.retired = true;
+        await update_gambler(retiree);
+
+        let message = `${retiree.name} has been retired.`;
+        try {
+            await interaction.reply(message);
+        } catch (error) { console.error(error) }
+	}
+}
