e352c658218b881919f24563374f18333184bcd0
Juha Ritakoski <juha.ritakoski@hotmail.com>
2024-04-01 14:02:17 +0300

    put /help hint in bot activity

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fs = require('node:fs');
 const path = require('node:path');
-const { Client, Collection, Events, GatewayIntentBits, Partials } = require('discord.js');
+const { Client, Collection, Events, GatewayIntentBits, Partials, ActivityType } = require('discord.js');
 const { token } = require('./config.json');
 const init_emojis = require("./functions/init_emojis");
 
@@ -27,6 +27,7 @@ for (const file of commandFiles) {
 
 client.once(Events.ClientReady, readyClient => {
 	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+	client.user.setActivity('/help', { type: ActivityType.Listening });
 });
 
 client.on(Events.InteractionCreate, async (interaction, user) => {
